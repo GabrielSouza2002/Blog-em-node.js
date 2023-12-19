738ecc7ccbfbacfd083a52e3e7c074bf47055f38
Gabriel Martins <gabrielmartins8826@hotmail.com>
2023-12-18 21:51:36 -0300

    sistema de paginação completo

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -97,7 +97,7 @@ router.get("/articles/page/:num", (req, res) => {
     if(isNaN(page) || page == 1){
         offset = 0;
     }else{
-        offset = parseInt(page) * 4;
+        offset = (parseInt(page) - 1 ) * 4;
     }
 
     Article.findAndCountAll({
@@ -113,6 +113,7 @@ router.get("/articles/page/:num", (req, res) => {
         }
 
         var result = {
+            page: parseInt(page),
             next: next,
             articles : articles
         }
